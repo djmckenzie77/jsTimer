80ae5c5dfd3a38ab76329c7cd3fdbeff98d34aaa
djmckenzie77 <djmckenzie@madisonacademy.com>
2019-01-25 07:24:29 -0600

    reset button update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 var h1 = document.getElementsByTagName("h1")[0],
   start = document.getElementById("start"),
   stop = document.getElementById("stop"),
-  clear = document.getElementById("clear"),
+  reset = document.getElementById("reset"),
   seconds = 0,
   minutes = 0,
   hours = 0,
@@ -41,7 +41,7 @@ stop.onclick = function() {
 };
 
 /* Clear button */
-clear.onclick = function() {
+reset.onclick = function() {
   h1.textContent = "00:00:00";
   seconds = 0;
   minutes = 0;
